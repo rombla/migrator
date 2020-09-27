71e7a0299dd43e184d6716eb771b60c3a6a1ffaf
rom1bl <romain.blanc@hey.com>
2020-09-27 22:50:40 +0200

    added Spotify specific types

--- types.ts
+++ types.ts
@@ -9,4 +9,32 @@ interface Playlist {
     items: Track[];
 }
 
-export { Track, Playlist };
+interface SpotifyTrack {
+    album: any;
+    artists: any;
+    available_markets: any;
+    disc_number: Number;
+    duration_ms: Number;
+    explicit: boolean;
+    external_ids: any;
+    external_urls: any;
+    href: string;
+    id: string;
+    is_playable: boolean;
+    linked_from: any;
+    restrictions: any;
+    name: string;
+    popularity: Number;
+    preview_url: string;
+    track_number: Number;
+    type: string;
+    uri: string;
+    is_local: boolean;
+}
+
+interface SpotifyTracksList {
+    added_at: Date;
+    track: SpotifyTrack;
+}
+
+export { Track, Playlist, SpotifyTrack, SpotifyTracksList };
